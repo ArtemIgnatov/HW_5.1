d441491ede083ab9f0564ef539114d10894d7d64
Artem Ignatov <ghettoarabdog@gmail.com>
2022-07-03 21:31:17 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_5._1
+{
+    internal class Program
+    {
+        static void Print(string[] nums)
+        {
+            foreach (var num in nums)
+            {
+                Console.WriteLine($"{num}");
+            }
+        }
+
+        static string[] Get(string Str)
+        {
+            string[] strNew = Str.Split(' ','.',',','?','!');
+            return strNew;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст.");
+            string a = Console.ReadLine();
+            string[] subs = Get(a);
+            Print(subs);    
+            
+            Console.ReadKey();
+        }
+        
+
+      
+    }   
+        
+}
